206bb46fb0ead8259c305a84154b298d7836fa4b
mutotu <muttalip.aydemir@gmail.com>
2024-03-19 18:25:32 -0700

    added func for pinecone

--- main.py
+++ main.py
@@ -41,5 +41,74 @@ def chunk_data(data, chunk_size=256):
   return chunks
   
   
-# chunks = chunk_data(load_document('./us_constitution.pdf'))
+chunks = chunk_data(load_document('./us_constitution.pdf'))
 # print(chunks)
+
+
+def print_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model('text-embedding-3-small')
+    total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
+    # check prices here: https://openai.com/pricing
+    print(f'Total Tokens: {total_tokens}')
+    print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.00002:.6f}')
+    
+
+
+def insert_or_fetch_embeddings(index_name, chunks):
+    # importing the necessary libraries and initializing the Pinecone client
+    import pinecone
+    from langchain_community.vectorstores import Pinecone
+    from langchain_openai import OpenAIEmbeddings
+    from pinecone import PodSpec
+
+    
+    pc = pinecone.Pinecone()
+        
+    embeddings = OpenAIEmbeddings(model='text-embedding-3-small', dimensions=1536)  # 512 works as well
+
+    # loading from existing index
+    if index_name in pc.list_indexes().names():
+        print(f'Index {index_name} already exists. Loading embeddings ... ', end='')
+        vector_store = Pinecone.from_existing_index(index_name, embeddings)
+        print('Ok')
+    else:
+        # creating the index and embedding the chunks into the index 
+        print(f'Creating index {index_name} and embeddings ...', end='')
+
+        # creating a new index
+        pc.create_index(
+            name=index_name,
+            dimension=1536,
+            metric='cosine',
+            spec=PodSpec(
+                environment='gcp-starter'
+            )
+        )
+
+        # processing the input documents, generating embeddings using the provided `OpenAIEmbeddings` instance,
+        # inserting the embeddings into the index and returning a new Pinecone vector store object. 
+        vector_store = Pinecone.from_documents(chunks, embeddings, index_name=index_name)
+        print('Ok')
+        
+    return vector_store
+    
+    
+def delete_pinecone_index(index_name='all'):
+  import pinecone
+  pc = pinecone.Pinecone()
+  
+  if index_name == 'all':
+    indexes = pc.list_indexes().names()
+    print('Deleting all indexes ... ')
+    for index in indexes:
+        pc.delete_index(index)
+    print('Ok')
+  else:
+    print(f'Deleting index {index_name} ...', end='')
+    pc.delete_index(index_name)
+    print('Ok')
+    
+delete_pinecone_index()    
+
+vector_store = insert_or_fetch_embeddings('askadocument', chunks)
